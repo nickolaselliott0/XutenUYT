99b73053bb36e133855d734441baeba9c71a2f7a
nickolaselliott0 <104137771+nickolaselliott0@users.noreply.github.com>
2023-02-16 13:53:15 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -5,7 +5,6 @@ import torchaudio
 
 def get_mel_from_wav(audio, _stft):
     audio = torch.clip(torch.FloatTensor(audio).unsqueeze(0), -1, 1)
-    audio = torch.autograd.Variable(audio, requires_grad=False)
     melspec, log_magnitudes_stft, energy = _stft.mel_spectrogram(audio)
     melspec = torch.squeeze(melspec, 0).numpy().astype(np.float32)
     log_magnitudes_stft = (
@@ -39,7 +38,6 @@ def pad_wav(waveform, segment_length):
     elif waveform_length > segment_length:
         return waveform[:segment_length]
     elif waveform_length < segment_length:
-        temp_wav = np.zeros((1, segment_length))
         temp_wav[:, :waveform_length] = waveform
     return temp_wav
 
